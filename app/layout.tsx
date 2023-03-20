4487204615102752d9f745e80eebd182b923129d
Joe.Wang <joe@rethk.com>
2023-03-20 15:13:39 +0800

    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import Navbar from '@/components/navbar'
+import Footer from '@/components/footer'
 
 export const metadata = {
   title: {
@@ -20,9 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Navbar/>
         <main className="relative z-10 grid grid-cols-[1fr,min(640px,100%),1fr] gap-y-8 px-4 font-sans text-base xl:grid-cols-[1fr,minmax(auto,240px),min(640px,100%),minmax(auto,240px),1fr] xl:gap-x-9 xl:px-0 [&>*]:col-start-2 xl:[&>*]:col-start-3">
+          <Navbar/>
           {children}
+          <Footer/>
         </main>
       </body>
     </html>
---
+++ components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+	return (
+		<footer className="text-xs text-gray-300 text-center py-10 md:my-20">
+			{` ${new Date().getFullYear()} © Longjun Wang.`}
+		</footer>
+	)
+}
